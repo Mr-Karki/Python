2296d722844bad9fb1159b6a32ba3e59718df58e
Mr-Karki <dikshitkarki26@gmail.com>
2025-05-19 13:21:28 +0545

    second day

---
+++ LearnPY/Day1/datatypes.py
@@ -0,0 +1,19 @@
+# keywords --> predefined special words with certain meaning on program
+# Identifiers --> what you can ann can not use as a variable
+
+age = 65
+print(age)
+#age gets rewritten
+age = 22
+print(age)
+
+# Formatting
+
+user_input = int(input("Enter number: "))
+print("Printing the Table", user_input)
+
+print(f"{user_input}*1={user_input*1}")
+print(f"{user_input}*2={user_input*2}")
+print(f"{user_input}*5={user_input*5}")
+print(f"{user_input}*10={user_input*10}")
+

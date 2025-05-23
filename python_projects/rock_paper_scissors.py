d590672212613389ac82cf8ad51aa46cc34c6492
Mr-Karki <dikshitkarki26@gmail.com>
2025-05-23 12:29:18 +0545

    Fix files

--- python_projects/rock_paper_scissors.py
+++ python_projects/rock_paper_scissors.py
@@ -1,16 +1,22 @@
-import random
+# Best of 3 for rock, paper, scissors game
+
+import random # This statement makes the functions defined in the random module available for use
 
 print("Let's play rock, paper, or scissors")
 
 player_wins = 0
 computer_wins = 0
 
+#for player to choose
 while player_wins < 2 and computer_wins < 2:
   player_choice = input("\nChoose rock, paper, or scissors: ").lower()
   choices = ["rock", "paper", "scissors"]
+
+#for computer to choose
   computer_choice = random.choice(choices)
   print(f"Computer chose: {computer_choice}")
 
+#condition to win match
   if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
     winner = "Player"
   elif player_choice == computer_choice:
@@ -18,6 +24,7 @@ while player_wins < 2 and computer_wins < 2:
   else:
     winner = "Computer"
 
+#condition to track first to win two match
   if winner == "Player":
     player_wins += 1
     print("You won")
